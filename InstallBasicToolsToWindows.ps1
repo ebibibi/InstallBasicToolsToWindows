a707c574fdd7f0a5191f8b37a24615efb91a7fbb
Masahiko Ebisuda <ebibibi@gmail.com>
2019-01-07 16:48:25 +0900

    disable IE ESC install chorome

---
+++ InstallBasicToolsToWindows.ps1
@@ -0,0 +1,30 @@
+#---------------
+#disable IE ESC
+#---------------
+function Disable-IEESC
+{
+$AdminKey = “HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}”
+$UserKey = “HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}”
+Set-ItemProperty -Path $AdminKey -Name “IsInstalled” -Value 0
+Set-ItemProperty -Path $UserKey -Name “IsInstalled” -Value 0
+Stop-Process -Name Explorer
+}
+Disable-IEESC
+
+#--------------
+#install chrome
+#--------------
+$LocalTempDir = $env:TEMP
+$ChromeInstaller = "ChromeInstaller.exe"
+(new-object    System.Net.WebClient).DownloadFile('http://dl.google.com/chrome/install/375.126/chrome_installer.exe', "$LocalTempDir\$ChromeInstaller")
+& "$LocalTempDir\$ChromeInstaller" /silent /install
+$Process2Monitor =  "ChromeInstaller"
+Do { 
+    $ProcessesFound = Get-Process | Where-Object{$Process2Monitor -contains $_.Name} | Select-Object -ExpandProperty Name
+    If ($ProcessesFound) {
+        "Still running: $($ProcessesFound -join ', ')" | Write-Host
+        Start-Sleep -Seconds 2 
+    } else {
+        Remove-Item "$LocalTempDir\$ChromeInstaller" -ErrorAction SilentlyContinue -Verbose 
+    } 
+} Until (!$ProcessesFound)
